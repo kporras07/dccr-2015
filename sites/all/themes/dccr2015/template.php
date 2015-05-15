f107bc0be61aa2c97e8c5812a9c7ff5041560585
Kevin Porras <kporras07@gmail.com>
2015-05-15 15:16:19 -0600

    Initial theme setup.

---
+++ sites/all/themes/dccr2015/template.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @file
+ * template.php file for dccr2015 theme.
+ */
+
+/**
+ * Add body classes if certain regions have content.
+ */
+function dccr2015_preprocess_html(&$variables) {
+}
+
+/**
+ * Override or insert variables into the page template for HTML output.
+ */
+function dccr2015_process_html(&$variables) {
+}
+
+/**
+ * Override or insert variables into the page template.
+ */
+function dccr2015_process_page(&$variables) {
+}
+
+/**
+ * Implements hook_preprocess_maintenance_page().
+ */
+function dccr2015_preprocess_maintenance_page(&$variables) {
+}
+
+/**
+ * Override or insert variables into the maintenance page template.
+ */
+function dccr2015_process_maintenance_page(&$variables) {
+}
+
+/**
+ * Override or insert variables into the node template.
+ */
+function dccr2015_preprocess_node(&$variables) {
+}
+
+/**
+ * Override or insert variables into the block template.
+ */
+function dccr2015_preprocess_block(&$variables) {
+}
+
+/**
+ * Implements theme_menu_tree().
+ */
+function dccr2015_menu_tree($variables) {
+}
+
+/**
+ * Implements theme_field__field_type().
+ */
+function dccr2015_field__taxonomy_term_reference($variables) {
+}
